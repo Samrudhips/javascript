14f329da955149b7f8df37c1192e7aa7a824311b
Samrudhips <20cs134.samrudhi@sjec.ac.in>
2024-11-23 10:58:21 +0530

    2311

---
+++ FatArrow/five.js
@@ -0,0 +1,10 @@
+// let wish=()=>{
+//     console.log(this);
+    
+// }
+// wish(); empty object
+function wish(){
+    console.log(this);
+    
+}
+wish();//object context-heavy
---
+++ FatArrow/six.js
@@ -0,0 +1,21 @@
+// function greet(){
+//     console.log("GM");
+    
+// }
+// greet()
+// function greet(){
+//     console.log("GN");
+    
+// }
+// greet() // function overriding 
+var greet=()=>{
+    console.log("GM");
+    
+}
+greet()
+var greet=()=>{
+    console.log("GN");
+    
+}
+greet()
+//Overriding is not possible
