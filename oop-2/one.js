8a4562882abf74a40638f969979dbae22aff2b91
Samrudhips <20cs134.samrudhi@sjec.ac.in>
2024-12-04 11:13:44 +0530

    04/12/2024

---
+++ oop-2/one.js
@@ -0,0 +1,4 @@
+class Parent{}
+class Child{}
+let c1=new Child()
+console.log(c1);
---
+++ oop-2/two.js
@@ -0,0 +1,8 @@
+class Parent{
+    assets="100cr"
+}
+class Child extends Parent{
+    qualities="good"
+}
+let c1=new Child()
+console.log(c1);
